8627d878d4d56264e3822920c0657626cb509446
Paul Dariye <paul.dariye@gmail.com>
2015-05-25 04:24:39 -0400

    update

--- Reeel/ScreeningDetailViewController.m
+++ Reeel/ScreeningDetailViewController.m
@@ -8,14 +8,22 @@
 
 #import "ScreeningDetailViewController.h"
 #import "RSVPDetailViewController.h"
-#import "ScreeningScrollContentViewController.h"
 #import <FXBlurView/FXBlurView.h>
 
 @interface ScreeningDetailViewController () <UINavigationControllerDelegate, UIScrollViewDelegate>
 @property (strong, nonatomic) UIImageView *screeningImageView;
 @property (strong, nonatomic) UIScrollView *scrollView;
-@property (strong, nonatomic) ScreeningScrollContentViewController *contentView;
+@property (strong, nonatomic) UIView *scrollContentView;
+@property (strong, nonatomic) UIImageView *gradientImageView;
+@property (strong, nonatomic) UIImageView *upIconImageView;
+@property (strong, nonatomic) UIButton *rsvpButton;
 @property (strong, nonatomic) FXBlurView *blurScreeningImageView;
+@property (assign, nonatomic) CGFloat lastContentOffset;
+@property (strong, nonatomic) UILabel *titleLabel;
+@property (strong, nonatomic) UILabel *synopsisLabel;
+@property (strong, nonatomic) UILabel *metaLabel;
+@property (strong, nonatomic) UILabel *directorsLabel;
+@property (strong, nonatomic) UILabel *starsLabel;
 
 @end
 
@@ -23,7 +31,7 @@
 
 @synthesize screening = _screening;
 @synthesize scrollView = _scrollView;
-@synthesize contentView = _contentView;
+
 
 - (UIScrollView *)scrollView
 {
@@ -33,7 +41,8 @@
     return _scrollView;
 }
 
-- (IBAction)RSVPButtonPressed:(UIButton *)sender {
+- (IBAction)RSVPButtonPressed:(UIButton *)sender
+{
     RSVPDetailViewController *rsvpDetail = [[RSVPDetailViewController alloc] init];
     
     rsvpDetail.screening = self.screening;
@@ -44,22 +53,107 @@
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
     CGFloat y = scrollView.contentOffset.y;
-    self.blurScreeningImageView.alpha = y / [UIScreen mainScreen].bounds.size.height;
+    
+    if (self.lastContentOffset > scrollView.contentOffset.y) {
+        self.blurScreeningImageView.alpha = y / ([UIScreen mainScreen].bounds.size.height * 0.65);
+        self.gradientImageView.alpha = y / ([UIScreen mainScreen].bounds.size.height * 0.5);
+        self.upIconImageView.alpha = y / ([UIScreen mainScreen].bounds.size.height);
+        self.lastContentOffset = scrollView.contentOffset.y;
+    } else if (self.lastContentOffset < scrollView.contentOffset.y) {
+        NSLog(@"value--> %f ", ([UIScreen mainScreen].bounds.size.height * 0.2)/y);
+        self.gradientImageView.alpha = ([UIScreen mainScreen].bounds.size.height)/y;
+        self.upIconImageView.alpha = ([UIScreen mainScreen].bounds.size.height * 0.5)/y;
+        self.lastContentOffset = scrollView.contentOffset.y;
+    }
+    
 }
 
 - (void)initContent
 {
-    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(20, [UIScreen mainScreen].bounds.size.height - 50, [UIScreen mainScreen].bounds.size.width, 21)];
-    title.text = @"Jason Ji";
+    UIImage *gradient = [UIImage imageNamed:@"gradient"];
+    self.gradientImageView = [[UIImageView alloc] initWithImage:gradient];
+    self.gradientImageView.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height * 1.5);
+    self.gradientImageView.clipsToBounds = YES;
+    self.gradientImageView.tintColor = [UIColor blackColor];
+    self.gradientImageView.contentMode = UIViewContentModeScaleAspectFill;
     
-    [self.scrollView addSubview:title];
-}
+    [self.scrollView addSubview:self.gradientImageView];
+    
+    UIImage *upIconImage = [UIImage imageNamed:@"up"];
+    self.upIconImageView = [[UIImageView alloc] initWithImage:upIconImage];
+    self.upIconImageView.frame = CGRectMake((self.scrollView.bounds.size.width - self.upIconImageView.frame.size.width)/2.0, [UIScreen mainScreen].bounds.size.height - 280, 25, 15);
+    [self.scrollView addSubview:self.upIconImageView];
+    
+    self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(25, [UIScreen mainScreen].bounds.size.height - 250, [UIScreen mainScreen].bounds.size.width - 50, 21)];
+    
+    NSDateFormatter *year = [[NSDateFormatter alloc] init];
+    [year setDateFormat:@"yyyy"];
 
+    self.titleLabel.text = [NSString stringWithFormat:@"%@ (%@)", [self.screening objectForKey:@"screeningTitle"], [year stringFromDate:[self.screening objectForKey:@"screeningReleaseDate"]]];
+    self.titleLabel.font = [UIFont systemFontOfSize:30];
+    self.titleLabel.textColor = [UIColor whiteColor];
+    self.titleLabel.numberOfLines = 0;
+    [self.titleLabel sizeToFit];
+    [self.scrollView addSubview:self.titleLabel];
+    
+    self.metaLabel = [[UILabel alloc] initWithFrame:CGRectMake(25, [UIScreen mainScreen].bounds.size.height - 205, [UIScreen mainScreen].bounds.size.width - 50, 21)];
+    self.metaLabel.text = [NSString stringWithFormat:@"%@ | %@ | %@", [self.screening objectForKey:@"screeningContentRating"], [self.screening objectForKey:@"screeningDuration"], [self.screening objectForKey:@"screeningGenre"]];
+    self.metaLabel.textColor = [UIColor whiteColor];
+    self.metaLabel.numberOfLines = 0;
+    [self.metaLabel sizeToFit];
+    [self.scrollView addSubview:self.metaLabel];
+    
+    UIImageView *horizonatalLine = [[UIImageView alloc] initWithFrame:CGRectMake(25, [UIScreen mainScreen].bounds.size.height - 180, self.metaLabel.frame.size.width, 1)];
+    horizonatalLine.backgroundColor = [UIColor lightTextColor];
+    [self.scrollView addSubview:horizonatalLine];
+    
+    
+    self.synopsisLabel = [[UILabel alloc] initWithFrame:CGRectMake(25, [UIScreen mainScreen].bounds.size.height - 160, [UIScreen mainScreen].bounds.size.width - 50, 21)];
+    self.synopsisLabel.text = [self.screening objectForKey:@"screeningSynopsis"];
+    self.synopsisLabel.textColor = [UIColor whiteColor];
+    self.synopsisLabel.numberOfLines = 0;
+    [self.synopsisLabel sizeToFit];
+    [self.scrollView addSubview:self.synopsisLabel];
+    
+    
+    self.directorsLabel = [[UILabel alloc] initWithFrame:CGRectMake(25, [UIScreen mainScreen].bounds.size.height - (self.synopsisLabel.frame.size.height + 30), [UIScreen mainScreen].bounds.size.width - 50, 21)];
+    self.directorsLabel.text = [NSString stringWithFormat:@"Director(s): %@", [self.screening objectForKey:@"screeningDirectorInfo"]];
+    self.directorsLabel.textColor = [UIColor whiteColor];
+    self.directorsLabel.numberOfLines = 0;
+    [self.directorsLabel sizeToFit];
+    [self.scrollView addSubview:self.directorsLabel];
+    
+    
+    
+    self.starsLabel = [[UILabel alloc] initWithFrame:CGRectMake(25, [UIScreen mainScreen].bounds.size.height - (self.directorsLabel.frame.size.height + 40), [UIScreen mainScreen].bounds.size.width - 50, 21)];
+    self.starsLabel.text = [NSString stringWithFormat:@"Star(s): %@", [self.screening objectForKey:@"screeningStarInfo"]];
+    self.starsLabel.textColor = [UIColor whiteColor];
+    self.starsLabel.numberOfLines = 0;
+    [self.starsLabel sizeToFit];
+    [self.scrollView addSubview:self.starsLabel];
+    
+    
+    self.rsvpButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.rsvpButton.frame = CGRectMake(25, [UIScreen mainScreen].bounds.size.height - (self.starsLabel.frame.size.height - 40), [UIScreen mainScreen].bounds.size.width - 50, 80);
+    [self.rsvpButton setContentMode:UIViewContentModeScaleAspectFit];
+    [self.rsvpButton setClipsToBounds:YES];
+    [self.rsvpButton setBackgroundColor:[UIColor redColor]];
+    [self.rsvpButton setTitle:@"RSVP" forState:UIControlStateNormal];
+    [self.rsvpButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.rsvpButton.titleLabel setFont:[UIFont systemFontOfSize:30]];
+    self.rsvpButton.layer.shadowOpacity = 0.3;
+    self.rsvpButton.layer.shadowRadius = 2.0f;
+    self.rsvpButton.layer.cornerRadius = 2.0f;
+    self.rsvpButton.center = CGPointMake([UIScreen mainScreen].bounds.size.width/2, [UIScreen mainScreen].bounds.size.height - (self.starsLabel.frame.size.height - 80));
+    
+    [self.rsvpButton addTarget:self action:@selector(RSVPButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [self.scrollView addSubview:self.rsvpButton];
+}
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    
     [self initContent];
+    [self.scrollView layoutIfNeeded];
 }
 
 - (void)viewDidLoad {
@@ -67,34 +161,9 @@
     
     [super viewDidLoad];
     
-    //contentView = [[ScreeningScrollContentViewController alloc] initWithNibName:@"ScreeningScrollContentViewController" bundle:nil];
-    
-    //contentView.screeningTitleLabel.text = [screening objectForKey:@"screeningTitle"];
-    
-    //[self.scrollView addSubview:contentView.scrollContentView];
-    
-    
-//    contentView.backgroundColor = [UIColor whiteColor];
-//    contentView.contentMode = UIViewContentModeScaleAspectFit;
-//    
-//    [scrollView addSubview:contentView];
-    
-    
-
-    
-//    scrollContentView.screeningTitleLabel.text = [screening objectForKey:@"screeningTitle"];
-//    
-//   
-//    
-//    scrollContentView.screeningTitleLabel.textColor = [UIColor whiteColor];
-//    scrollContentView.screeningTitleLabel.font = [UIFont systemFontOfSize:28];
-//
-//    [scrollView addSubview:scrollContentView.view];
-    
-//    scrollView = [CustomScreeningScrollViewController new];
-    
-    
     self.navigationItem.title = [_screening objectForKey:@"screeningTitle"];
+    
+    self.gradientImageView.alpha = 1;
     
     // Set background color of view
     self.view.backgroundColor = [UIColor whiteColor];
@@ -115,41 +184,13 @@
     self.blurScreeningImageView = [[FXBlurView alloc] initWithFrame:self.screeningImageView.frame];
     self.blurScreeningImageView.alpha = 0;
     [self.screeningImageView addSubview:self.blurScreeningImageView];
-    
+
     self.scrollView.delegate = self;
+    self.scrollView.showsVerticalScrollIndicator = NO;
     self.scrollView.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
-    self.scrollView.contentSize = CGSizeMake([UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height * 2);
+    self.scrollView.contentSize = CGSizeMake([UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height * 1.5);
     self.scrollView.backgroundColor = [UIColor clearColor];
     [self.view addSubview:self.scrollView];
-//
-//    [scrollView addSubview:contentView.view];
-//    
-    
-    
-
-//    [[NSBundle mainBundle] loadNibNamed:@"CustomScreeningScrollViewController" owner:scrollView options:nil];
-//    
-//    scrollView.screeningTitleLabel.text = [screening objectForKey:@"screeningTitle"];
-//    scrollView.screeningRatingLabel.text = [screening objectForKey:@"screeningContentRating"];
-//    
-//    
-//    // Styling
-//    scrollView.screeningTitleLabel.textColor = [UIColor whiteColor];
-//    scrollView.screeningTitleLabel.font = [UIFont systemFontOfSize:28];
-//    scrollView.screeningRatingLabel.textColor = [UIColor whiteColor];
-//    
-//    [self.view addSubview:scrollView.view];
-//    [[self movieRatingLabel] setText:[NSString stringWithFormat:@"Rating: %@ / 10",[screening objectForKey:@"screeningContentRating"]]];
-//    
-//    
-//    [[self movieSynopsisText] setText:[self.screening objectForKey:@"screeningSynopsis"]];
-//    
-//    [[self durationLabel] setText:[NSString stringWithFormat:@"%@ min",[screening objectForKey:@"screeningDuration"]]];
-//    
-//    NSDateFormatter *releaseDateFormat = [[NSDateFormatter alloc] init];
-//    [releaseDateFormat setDateFormat:@"MMM d y"];
-//    
-//    [[self releaseDateLabel] setText:[releaseDateFormat stringFromDate:[self.screening objectForKey:@"screeningReleaseDate"]]];
 
 }
 
